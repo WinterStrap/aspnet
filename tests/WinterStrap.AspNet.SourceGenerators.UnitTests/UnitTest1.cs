3753234118910c2d6fd7c3ebc3531a7244e1976a
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2023-03-09 17:52:55 +0100

    chore: create default structure

---
+++ tests/WinterStrap.AspNet.SourceGenerators.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WinterStrap.AspNet.SourceGenerators.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
