080f6a324351bced16d3d36ef0b513a49348db2e
Victor Pérez Ngounou <71229551+vngounou26@users.noreply.github.com>
2023-03-10 15:21:29 +0100

    Add injection dependancy

---
+++ src/WinterStrap.AspNet.SourceGenerators/ComponentModel/Attribute/ServiceInjectAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WinterStrap.AspNet.SourceGenerators.ComponentModel.Attribute;
+
+/// <summary>
+/// 
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class ServiceInjectAttribute:System.Attribute
+{
+    
+}
